6527237e54cbabfcab50817001b425342b0dc642
Andreas Hechler <info@andreashechler.com>
2022-11-18 15:36:46 +0100

    changed deploy script: added exclusions

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 set -e
 set -x
 
-rsync --recursive --verbose --delete public/ andy@leo.uberspace.de:/home/andy/code/imageboard
+rsync --recursive --verbose --delete --exclude ".git" --exclude "node_modules" --exclude "uploads" --exclude "secrets.json" . andy@leo.uberspace.de:/home/andy/code/imageboard
+ssh andy@leo.uberspace.de "supervisorctl stop imageboard && cd ~/code/imageboard && npm install && supervisorctl start imageboard"
